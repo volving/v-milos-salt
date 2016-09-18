6ce96f3a3c25c183e374370bf7efe223eb659b76
volving <volving@icloud.com>
2016-09-18 10:02:12 +0800

    Add: slider in comparison

--- public/js/runslider.js
+++ public/js/runslider.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function readURL(input) {
 
     if (input.files && input.files[0]) {
@@ -5,7 +7,7 @@ function readURL(input) {
 
         reader.onload = function(e) {
             $('#blah').attr('src', e.target.result);
-        }
+        };
 
         reader.readAsDataURL(input.files[0]);
     }
