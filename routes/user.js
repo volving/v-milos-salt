9a5b843198e0f094bd9cc8905cd66f71f02aa779
volving <volving@icloud.com>
2016-09-28 18:53:28 +0800

    Add: 用户资料相关

--- routes/user.js
+++ routes/user.js
@@ -4,18 +4,38 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
 
+router.get('/', function (req, res, next) { //jshint ignore: line
+    var query = req.query;
+    User.findOne({
+        _id: query.id
+    }).exec(function (err, user) {
+        if (err) {
+            req.flash('warning', '该用户不存在!');
+            return res.redirect('/');
+        }
+
+        return res.render('user/profile', {
+            user: user
+        });
+    });
+});
 
 /* GET users listing. */
-router.get('/:id', function (req, res, next) { //jshint ignore:line
-    var id = req.params.id;
-    if (!id) {
-        req.flash('warning', 'URL无效!');
-        return res.redirect('/');
-    }
-    // User.findOne({
-    //     _id: id
-    // })
-    // res.send('respond with a resource');
+router.get('/save', function (req, res, next) { //jshint ignore:line
+    var query = req.query;
+
+    User.findOne({
+        _id: id
+    }).exec(function (err, user) {
+        if (err) {
+            req.flash('warning', '该用户不存在!');
+            return res.redirect('/');
+        }
+
+        return res.render('user/profile', {
+            user: user
+        });
+    });
 });
 
 module.exports = router;
