5b05ba675d74afc92ffe47f5042469186ccf480c
volving <volving@icloud.com>
2016-09-29 16:31:47 +0800

    Add: 添加Object to Querystring 方法

--- bin/utils.js
+++ bin/utils.js
@@ -5,5 +5,14 @@ var mongoose = require('mongoose');
 module.exports = {
     getMongoId: function getMongoId(str) {
         return mongoose.Types.ObjectId(str);
+    },
+    formatQuery: function (obj) {
+        var str = [];
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+            }
+            return str.join('&');
+        }
     }
 };
